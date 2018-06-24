137adf557b035c2a46a9e7d2af220c6cce33ffaa
jasmaa <jasonmaa3955@gmail.com>
2018-06-23 20:31:28 -0400

    add UI and time

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,7 +2,18 @@
 
 extends Node2D
 
+enum GameState {
+	TITLE,
+	GAME
+}
+var game_state
+
 var player_points = 0
+var time_left = 100
 
 func _ready():
-	pass
+	game_state = GameState.GAME
+
+func _process(delta):
+	if game_state == GameState.GAME:
+		time_left -= delta
--- Scripts/HUD.gd
+++ Scripts/HUD.gd
@@ -1,13 +1,16 @@
 extends CanvasLayer
 
 onready var score_label = $GUI/VBoxContainer/Score
+onready var time_label = $GUI/VBoxContainer/Time
 
 func _ready():
 	score_label.get_font("font").create_from_fnt("res://Font/custom.fnt")
-	pass
+	time_label.get_font("font").create_from_fnt("res://Font/custom.fnt")
 
 func _process(delta):
-	score_label.text = points2score(GameManager.player_points)
+	score_label.text = "SCORE:" + num2label(GameManager.player_points)
+	time_label.text = "TIME: " + num2label(int(GameManager.time_left))
+	
 
-func points2score(p):
-	return  String(p / 1000) + String(p / 100) + String(p / 10) + String(p % 10)
+func num2label(p):
+	return  String((p/1000) % 10) + String((p/100) % 10) + String((p/10) % 10) + String(p % 10)
